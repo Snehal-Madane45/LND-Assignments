a32c5a35ccfd874f87018a615b03cb691bbd5c0a
Snehal Madane <103511153+Snehal-Madane45@users.noreply.github.com>
2022-06-24 13:43:04 +0530

    Add files via upload

---
+++ Assignment3.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3._2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           Stack<int> stack = new Stack<int>(); 
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);           
+            stack.Push(4);
+
+            foreach (int i in stack)
+            {
+                Console.WriteLine("Push Element in Array:"+i+",");
+            }
+            stack.Pop();
+            stack.Pop();
+            foreach (int i in stack)
+            {
+                Console.WriteLine("After Pop ,Element in Array:" + i + ",");
+            }
+
+        }
+    }
+}
